980a921a5063ebdbd9d057d975316071effb862b
Mario Ortega <mariooc04@gmail.com>
2025-02-17 16:05:22 +0100

    implemented .h classes for different types of instructions

--- include/instructions.hh
+++ include/instructions.hh
@@ -41,23 +41,59 @@ class r_instruction : public instruction {
 };
 
 class i_instruction : public instruction {
-    // ...
+    public:
+        i_instruction(uint32_t bitstream) :
+            instruction(bitstream, type::i) {}
+        constexpr uint8_t rd() const { return bits(7, 5); }
+        constexpr uint8_t funct3() const { return bits(12, 3); }
+        constexpr uint8_t rs1() const { return bits(15, 5); }
+        constexpr uint8_t imm() const { return bits(20, 12); }
 };
 
 class s_instruction : public instruction {
-    // ...
+    public:
+        s_instruction(uint32_t bitstream) :
+            instruction(bitstream, type::s) {}
+        constexpr uint32_t imm() const { return ((bits(25, 7) << 5) | (bits(7, 5))); }
+        constexpr uint8_t funct3() const { return bits(12, 3); }
+        constexpr uint8_t rs1() const { return bits(15, 5); }
+        constexpr uint8_t rs2() const { return bits(20, 5); }
 };
 
 class b_instruction : public instruction {
-    // ...
+    public:
+        b_instruction(uint32_t bitstream) :
+            instruction(bitstream, type::b) {}
+        constexpr uint8_t imm11() const { return bits(7,1); }
+        constexpr uint8_t imm1_4() const { return bits(8,4); }
+        constexpr uint8_t funct3() const { return bits(12, 3); }
+        constexpr uint8_t rs1() const { return bits(15, 5); }
+        constexpr uint8_t rs2() const { return bits(20, 5); }
+        constexpr uint8_t imm5_10() const { return bits(25, 6); }
+        constexpr uint8_t imm12() const { return bits(31, 1); }
+
+        constexpr uint32_t imm() const {
+            return (imm12() << 12) | (imm11() << 11) | (imm5_10() << 5) | (imm1_4() << 1);
+        }   
 };
 
 class u_instruction : public instruction {
-    // ...
+    public:
+        u_instruction(uint32_t bitstream) :
+            instruction(bitstream, type::u) {}
+        constexpr uint8_t rd() const { return bits(7, 5); }
+        constexpr uint32_t imm() const { return (bits(12, 20) << 12); }
 };
 
 class j_instruction : public instruction {
-    // ...
+    public:
+        r_instruction(uint32_t bitstream) :
+            instruction(bitstream, type::r) {}
+        constexpr uint8_t rd() const { return bits(7, 5); }
+        constexpr uint8_t funct3() const { return bits(12, 3); }
+        constexpr uint8_t rs1() const { return bits(15, 5); }
+        constexpr uint8_t rs2() const { return bits(20, 5); }
+        constexpr uint32_t funct7() const { return bits(25, 7); }
 };
 
 
